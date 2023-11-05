e88e0185390ee50f5fc92dd64cf5d3b733a4832c
belousov-gi <belousov.gi@yandex.ru>
2023-11-05 13:57:44 +0700

    Added task 4

--- task_4/Program.cs
+++ task_4/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+
 
 namespace task_4
 {
@@ -12,6 +12,7 @@ namespace task_4
             string[] inputDataArr = inputData.Split(' ');
             int amountOfNumbers = int.Parse(inputDataArr[0]);
             int amountOfAvailableChanges = int.Parse(inputDataArr[1]);
+            long maxSubstraction = 0;
             
             string inputNumbers = Console.ReadLine();
             int[] numbers = Array.ConvertAll(inputNumbers.Split(' '),input => int.Parse(input));
@@ -19,7 +20,7 @@ namespace task_4
 
             Dictionary<int, List<int>> numbersByRank = new Dictionary<int, List<int>>();
             int currentNumberOfRank = 0;
-            int deviderForRank = 10;
+            long deviderForRank = 10;
 
             //devide numbers on ranks
             foreach (var value in numbers)
@@ -32,23 +33,63 @@ namespace task_4
 
                 try
                 {
-                    numbersByRank[currentNumberOfRank]?.Add(value);
-                }
-                
-                catch (KeyNotFoundException e)
-                {
-                    var addList = new List<int>();
-                    numbersByRank.Add(currentNumberOfRank, addList);
+                    if (!numbersByRank.ContainsKey(currentNumberOfRank))
+                    {
+                        var addList = new List<int>();
+                        numbersByRank.Add(currentNumberOfRank, addList);
+                    }
                     numbersByRank[currentNumberOfRank].Add(value);
                 }
-
-            }
-
-            //TODO: add logic with replacement
-            for (int i = currentNumberOfRank; i < 0; i--)
-            {
                 
+
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+
             }
+
+            int currentSubstraction;
+            for (; currentNumberOfRank >= 0 && amountOfAvailableChanges > 0; currentNumberOfRank--)
+            {
+                List<int> currentRankList = numbersByRank[currentNumberOfRank];
+                int tempDevider = (int)Math.Pow(10, currentNumberOfRank);
+                // int tempNumberForSubstr = Int32.Parse(CreateNumberForSubtraction(currentNumberOfRank));
+
+                for (int j = 0, maxIndex = currentRankList.Count - 1;
+                     j <= maxIndex && amountOfAvailableChanges > 0;
+                     j++ )
+                {
+                    int numberOfMaxRankInValue = currentRankList[j] / tempDevider;
+                    
+                    currentRankList[j] -= numberOfMaxRankInValue * tempDevider;
+                    
+                    if (numberOfMaxRankInValue == 9) { continue; }
+
+                    amountOfAvailableChanges--;
+                    currentSubstraction = (9 - numberOfMaxRankInValue) * tempDevider;
+                    maxSubstraction += currentSubstraction;
+                    
+                }
+                
+                //Add elements to list with less rank for future comparing
+                if (numbersByRank.ContainsKey(currentNumberOfRank - 1))
+                {
+                    numbersByRank[currentNumberOfRank - 1].AddRange(numbersByRank[currentNumberOfRank]);
+                    numbersByRank[currentNumberOfRank - 1].Sort();
+                    
+                }
+                else
+                {
+                    numbersByRank.Remove(currentNumberOfRank);
+                    currentRankList.Sort();
+                    numbersByRank[currentNumberOfRank - 1] = currentRankList;
+                }
+            }
+            
+            Console.WriteLine(maxSubstraction);
         }
+
     }
 }
